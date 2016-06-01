7877cca662c8ac980ff7b38ada0a9a3e1536fd06
Alexandr Krivonos <krausweb291985@gmail.com>
2016-06-01 18:44:42 +0300

    Sorted/init methods in function.js (part 1). Fix links.phtml - moved in local.xml. Add ultima_grid12. Add device background img. Add new grid class in the 1column.phtml

--- skin/frontend/sport2000/default/js/functions.js
+++ skin/frontend/sport2000/default/js/functions.js
@@ -26,130 +26,168 @@
  * @license     http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  */
 
+var bp = {
+    xsmall: 479,
+    small: 599,
+    medium: 770,
+    large: 979,
+    xlarge: 1199
+};
 
 $j(document).ready(function(){
-
-    //////////////// for account /////////////
-    var skipContents = $j('.skip-content');
-    var skipLinks = $j('.skip-link');
-    skipLinks.on('click', function(e){e.preventDefault()});
-
-    skipLinks.on('mouseenter mouseleave', function (e) {
-        e.preventDefault();
-
-        var self = $j(this);
-        // Use the data-target-element attribute, if it exists. Fall back to href.
-        var target = self.attr('data-target-element') ? self.attr('data-target-element') : self.attr('href');
-
-        // Get target element
-        var elem = $j(target);
-
-        // Check if stub is open
-        var isSkipContentOpen = elem.hasClass('skip-active') ? 1 : 0;
-
-        // Hide all stubs
-        skipLinks.removeClass('skip-active a-account-cart-hover');
-        skipContents.removeClass('skip-active');
-
-        // Toggle stubs
-        if (isSkipContentOpen) {
-            self.removeClass('skip-active a-account-cart-hover');
-        } else {
-            self.addClass('skip-active a-account-cart-hover');
-            elem.addClass('skip-active');
-        }
-    });
-
-    ////////////////// for Account Detail /////////////////////////
-    var accountDetail = $j('.account-detail');
-    var account = $j('.account');
-    accountDetail.on('mouseenter mouseleave', function () {
-        // Toggle stubs
-        if ( accountDetail.hasClass('account-detail-active') ) {
-            accountDetail.removeClass('skip-active account-detail-active');
-            account.removeClass('a-account-cart-hover');
-        } else {
-            accountDetail.addClass('skip-active account-detail-active');
-            account.addClass('a-account-cart-hover');
-        }
-    });
-
-    /////////////////// for Cart Detail ////////////////
-    var cartDetail = $j('.cart-detail');
-    var cart = $j('.cart');
-    cartDetail.on('mouseenter mouseleave', function () {
-        // Toggle stubs
-        if ( cartDetail.hasClass('cart-detail-active') ) {
-            cartDetail.removeClass('skip-active cart-detail-active');
-            cart.find('a.skip-link').removeClass('a-account-cart-hover');     // for cart - on 'a'
-        } else {
-            cartDetail.addClass('skip-active cart-detail-active');
-            cart.find('a.skip-link').addClass('a-account-cart-hover');        // for cart - on 'a'
-        }
-    });
+    headerCustomSport2000.init();
+});
 
 
-    /////////////////// for search /////////////////
-    var searchInput = $j('#search');
-    // if isset search data
-    if($j(searchInput).val()){
-        $j(searchInput).removeClass('search-input');
-        $j(searchInput).addClass('search-input-hasdata');
-    }
-    $j(searchInput).on('blur', function(){
-        if($j(this).val()) $j(this).addClass('search-input-hasdata');
-    });
-    $j(searchInput).on('focus', function(){
-        $j(this).removeClass('search-input-hasdata');
-    });
-    $j('#btn-search').on('click', function (e) {
-        if(!$j(searchInput).val()) {
+
+
+var headerCustomSport2000 = {
+    init: function(){
+        this.accountHoverVisibleHide();
+        this.accountDetailHoverVisibleHide();
+        this.cartDetailHoverVisibleHide();
+        this.searchInputVisibleHide();
+        this.menuMiniHoverVisibleHideResize();
+    },
+
+    accountHoverVisibleHide: function(){
+        var accountSkipContents = $j('.skip-content')
+            , accountSkipLinks = $j('.skip-link')
+            , accountSkipActiveCss = 'skip-active'
+            , accountSkipActiveCartHoverCss = 'skip-active a-account-cart-hover';
+
+        // canceled touch for tablet/mobile
+        accountSkipLinks.on('click', function(e){e.preventDefault()});
+
+        // hover visible/hide account
+        accountSkipLinks.on('mouseenter mouseleave', function (e) {
             e.preventDefault();
-            $j(searchInput).toggleClass('search-input', 500);
-        }
-    });
-    $j(document).mouseup(function (e) {
-        var search_form = $j("#search_mini_form");
-        if (search_form.has(e.target).length === 0){
-            if(!$j(searchInput).val()) $j(searchInput).addClass('search-input');
-        }
-    });
 
+            var self = $j(this);
+            // Use the data-target-element attribute, if it exists. Fall back to href.
+            var target = self.attr('data-target-element') ? self.attr('data-target-element') : self.attr('href');
+            // Get target element
+            var elem = $j(target);
+            // Check if stub is open
+            var isSkipContentOpen = elem.hasClass(accountSkipActiveCss) ? 1 : 0;
 
-    //////////////////// for mini menu, after 868 px ///////////////////
-    /////////////////// for Cart Detail ////////////////
-    var menuMini = $j('.page-header-menu-mini');
-    var menuTop = $j('.page-header-container-top-menu');
-    var showMiniMenu = 848 //(@media max-width 868-20)
-        , maxTabletSize = 768 // 755 - for PC (
-        , minTabletSize = 500
-        , paddingMinTablet = 10
-        , paddingMaxTablet = 15
-        , paddingDefault = 30;
+            // Hide all stubs
+            accountSkipLinks.removeClass(accountSkipActiveCartHoverCss);
+            accountSkipContents.removeClass(accountSkipActiveCss);
 
-    menuMini.on('mouseenter mouseleave', function () {
-        // Toggle stubs
-        if ( menuMini.hasClass('active') && !menuMini.hasClass('active-top') ) {
-            menuMini.removeClass('active');
-            menuTop.removeClass('top-menu-mini-active');
-            // remove "align" right when not active mini menu
-            menuTop.removeAttr('style');
-        } else {
-            menuMini.addClass('active');
-            menuTop.addClass('top-menu-mini-active');
-
-            if( $j(window).width() < minTabletSize ){
-                // 10 //@media only screen and(max-width: 499px) {
-                menuTop.css('left', $j(window).width() - 130 - paddingMinTablet);
-            }else if( $j(window).width() < maxTabletSize ){
-                // 15 //@media only screen and(max-width: 770px) {
-                menuTop.css('left', $j(window).width() - 130 - paddingMaxTablet);
-            }else{
-                menuTop.css('left', $j(window).width() - 130 - paddingDefault);
+            // Toggle stubs
+            if (isSkipContentOpen) {
+                self.removeClass(accountSkipActiveCartHoverCss);
+            } else {
+                self.addClass(accountSkipActiveCartHoverCss);
+                elem.addClass(accountSkipActiveCss);
             }
-        }
-    });
+        });
+    },
+    accountDetailHoverVisibleHide: function(){
+        var account = $j('.account')
+            , accountDetail = $j('.account-detail')
+            , accountDetailActiveCss = 'account-detail-active'
+            , accountDetailSkipActiveDetailCss = 'skip-active account-detail-active'
+            , accountDetailHoverCss = 'a-account-cart-hover';
 
+        accountDetail.on('mouseenter mouseleave', function () {
+            // Toggle stubs
+            if ( accountDetail.hasClass(accountDetailActiveCss) ) {
+                accountDetail.removeClass(accountDetailSkipActiveDetailCss);
+                account.removeClass(accountDetailHoverCss);
+            } else {
+                accountDetail.addClass(accountDetailSkipActiveDetailCss);
+                account.addClass(accountDetailHoverCss);
+            }
+        });
+    },
+    cartDetailHoverVisibleHide: function(){
+        var cart = $j('.cart')
+            , cartDetail = $j('.cart-detail')
+            , cartDetailActiveCss = 'cart-detail-active'
+            , cartDetailSkipActiveDetailCss = 'skip-active cart-detail-active'
+            , cartDetailAskipLinkCss = 'a.skip-link'
+            , cartDetailAhoverCss = 'a-account-cart-hover';
+
+        cartDetail.on('mouseenter mouseleave', function () {
+            // Toggle stubs
+            if ( cartDetail.hasClass(cartDetailActiveCss) ) {
+                cartDetail.removeClass(cartDetailSkipActiveDetailCss);
+                cart.find(cartDetailAskipLinkCss).removeClass(cartDetailAhoverCss);
+            } else {
+                cartDetail.addClass(cartDetailSkipActiveDetailCss);
+                cart.find(cartDetailAskipLinkCss).addClass(cartDetailAhoverCss);
+            }
+        });
+    },
+    searchInputVisibleHide: function(){
+        var searchInput = $j('#search')
+            , searchInputBtn =  $j('#btn-search')
+            , searchInputForm =  $j("#search_mini_form")
+            , searchInputCss = 'search-input';
+
+        // visible input search
+        if(searchInput.val()) searchInput.removeClass(searchInputCss);
+        searchInputBtn.on('click', function (e) {
+            if(!searchInput.val()) {
+                e.preventDefault();
+                searchInput.toggleClass(searchInputCss, 500);
+            }
+        });
+        $j(document).mouseup(function (e) {
+            if (searchInputForm.has(e.target).length === 0){
+                if(!searchInput.val()) searchInput.addClass(searchInputCss);
+            }
+        });
+    },
+    menuMiniHoverVisibleHideResize: function(){
+        // for menu mini, after 868 px
+
+        //ПЕРЕНЕСТИ В КСС !!!!!!!!!!!!!!!!!!!!!!!
+        var menuMini = $j('.page-header-menu-mini');
+        var menuTop = $j('.page-header-container-top-menu');
+        var showMiniMenu = 848 //(@media max-width 868-20)
+            , maxTabletSize = 768 // 755 - for PC (
+            , minTabletSize = 500
+            , paddingMinTablet = 10
+            , paddingMaxTablet = 15
+            , paddingDefault = 30;
+        /*
+         xsmall: 479,
+         small: 599,
+         medium: 770,
+         large: 979,
+         xlarge: 1199
+         */
+        //ПЕРЕНОСИТЬ МЕНЮ ДЖАВАСКРИПТОМ, СДЕЛАТЬ КАК ПОЗИШН РЕЛАТИВ !!!!!!!!!!!!!!!!!!!!!
+
+        // hover ico menu
+        menuMini.on('mouseenter mouseleave', function () {
+            // Toggle stubs
+            if ( menuMini.hasClass('active') && !menuMini.hasClass('active-top') ) {
+                menuMini.removeClass('active');
+                menuTop.removeClass('top-menu-mini-active');
+                // remove "align" right when not active mini menu
+                menuTop.removeAttr('style');
+            } else {
+                menuMini.addClass('active');
+                menuTop.addClass('top-menu-mini-active');
+
+                if( $j(window).width() < minTabletSize ){
+                    // 10 //@media only screen and(max-width: 499px) {
+                    menuTop.css('left', $j(window).width() - 130 - paddingMinTablet);
+                }else if( $j(window).width() < maxTabletSize ){
+                    // 15 //@media only screen and(max-width: 770px) {
+                    menuTop.css('left', $j(window).width() - 130 - paddingMaxTablet);
+                }else{
+                    // 30
+                    menuTop.css('left', $j(window).width() - 130 - paddingDefault);
+                }
+            }
+        });
+
+        // hover detail menu
         menuTop.on('mouseenter mouseleave', function () {
             if($j(window).width() < showMiniMenu) {
                 if (menuTop.hasClass('top-menu-mini-active')) {
@@ -169,12 +207,12 @@ $j(document).ready(function(){
                         // 15 //@media only screen and(max-width: 770px) {
                         menuTop.css('left', $j(window).width() - 130 - paddingMaxTablet);
                     } else {
+                        // 30
                         menuTop.css('left', $j(window).width() - 130 - paddingDefault);
                     }
                 }
             }
         });
-    //////////////////////////////////////////////////////////////
+    }
 
-
-});
+};
